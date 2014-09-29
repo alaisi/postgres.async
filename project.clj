0c9bccb58b6a547a49e804d92e7971fc6ce52a6b
Antti Laisi <alaisi@iki.fi>
2014-09-29 13:24:41 +0300

    Release version 0.1.0

--- project.clj
+++ project.clj
@@ -1,8 +1,10 @@
-(defproject alaisi/postgres.async "0.1.0-SNAPSHOT"
+(defproject alaisi/postgres.async "0.1.0"
   :description "Asynchronous PostgreSQL Clojure client"
   :url "http://github.com/alaisi/postgres.async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :scm {:name "git"
+        :url "http://github.com/alaisi/postgres.async.git"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [com.github.alaisi.pgasync/postgres-async-driver "0.2"]]
