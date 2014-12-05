fc3124f87c29ef46be0903f5141ae550d93a2263
Antti Laisi <alaisi@iki.fi>
2014-12-05 21:42:11 +0200

    Next is 0.3.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alaisi/postgres.async "0.2.0"
+(defproject alaisi/postgres.async "0.3.0-SNAPSHOT"
   :description "Asynchronous PostgreSQL Clojure client"
   :url "http://github.com/alaisi/postgres.async"
   :license {:name "Eclipse Public License"
