24c0d5cb5b8e344728d2498fca8d216d9e6ccb0e
Antti Laisi <alaisi@iki.fi>
2015-05-06 00:18:20 +0300

    Upgrade postgres-async-driver dependency

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
         :url "http://github.com/alaisi/postgres.async.git"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [com.github.alaisi.pgasync/postgres-async-driver "0.4"]]
+                 [com.github.alaisi.pgasync/postgres-async-driver "0.5"]]
   :global-vars {*warn-on-reflection* true}
   :target-path "target/%s"
   :profiles {:dev {:source-paths ["dev"]
