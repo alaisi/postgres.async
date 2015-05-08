94e29c852b9d0dd6981f5cd918038a22753fa3d6
Antti Laisi <alaisi@iki.fi>
2015-05-08 21:40:48 +0300

    lein-release plugin: bumped version from 0.5.0 to 0.5.1-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject alaisi/postgres.async "0.5.0"
+(defproject alaisi/postgres.async "0.5.1-SNAPSHOT"
   :description "Asynchronous PostgreSQL Clojure client"
   :url "http://github.com/alaisi/postgres.async"
   :license {:name "Eclipse Public License"
