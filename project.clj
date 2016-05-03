85f65d0b4fae453e573dbb0c834f3f92e379c75e
Antti Laisi <alaisi@iki.fi>
2016-05-03 20:16:45 +0300

    Prepare for release.

--- project.clj
+++ project.clj
@@ -1,17 +1,17 @@
-(defproject alaisi/postgres.async "0.7.0-SNAPSHOT"
+(defproject alaisi/postgres.async "0.7.0"
   :description "Asynchronous PostgreSQL Clojure client"
   :url "http://github.com/alaisi/postgres.async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "http://github.com/alaisi/postgres.async.git"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
-                 [com.github.alaisi.pgasync/postgres-async-driver "0.8-SNAPSHOT"]
-                 [cheshire "5.5.0" :scope "provided"]]
+                 [com.github.alaisi.pgasync/postgres-async-driver "0.8"]
+                 [cheshire "5.6.1" :scope "provided"]]
   :lein-release {:deploy-via :clojars}
   :global-vars {*warn-on-reflection* true}
   :target-path "target/%s"
   :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.4"]
-                                  [org.clojure/java.classpath "0.2.0"]]}})
+                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
+                                  [org.clojure/java.classpath "0.2.3"]]}})
